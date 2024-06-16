4303eacb55526a01b31fd312b8bd314e134ce72a
Marcos Medina <mmedinaviva01>
2024-06-16 18:24:36 -0500

    feat(users): implement UsersService with create, findByEmail, and findById methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { createUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+
+    async create(createUserDto: createUserDto): Promise<User> {
+        const createdUser = new this.userModel(createUserDto);
+        return createdUser.save();
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        return await this.userModel.findOne({email}).select('-password')
+    }
+
+    async findById(userId: string): Promise<User> {
+        return await this.userModel.findById(userId).select('-password')
+    }
+}
