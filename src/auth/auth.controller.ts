5dc7d16f4e29e21690b300f48d17c3c24b81bafa
Marcos Medina <mmedinaviva01>
2024-06-16 18:28:11 -0500

    feat(auth): implement AuthController with checkEmail, login, and register endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CheckEmailDto } from './dto/checkEmail.dto';
+import { LoginDto } from './dto/login.dto';
+import { createUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('check-email')
+    async checkEmail(@Body() checkEmailDto: CheckEmailDto) {
+        return this.authService.checkEmail(checkEmailDto)
+    }
+
+    @Post('login')
+    async login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto)
+    }
+
+    @Post('register')
+    async register(@Body() createUserDto: createUserDto) {
+        return this.authService.register(createUserDto)
+    }
+}
