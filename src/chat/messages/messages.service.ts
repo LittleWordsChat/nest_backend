24a79cf8c5e41f80eb0831498a3f9aac6df82c2e
Marcos Medina <mmedinaviva01>
2024-06-17 12:31:34 -0500

    feat(chat): implement MessagesService for managing message logic

---
+++ src/chat/messages/messages.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Message } from '../schemas/message.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class MessagesService {
+    constructor(@InjectModel(Message.name) private messageModel: Model<Message>) { }
+
+    async createMessage(data: any): Promise<Message> {
+        const message = new this.messageModel(data)
+        return message.save()
+    }
+
+    async markMessageAsSeen(userId: string, msgByUserId: string): Promise<void> {
+        await this.messageModel.updateMany(
+            {
+                msgBYUserId: msgByUserId,
+                seen: false
+            },
+            {
+                $set: { seen: true }
+            }
+        )
+    }
+}
