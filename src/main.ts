0c32a47868571ec7770cb9b1baca152b6b7d56e3
Marcos Medina <mmedinaviva01>
2024-06-16 21:27:40 -0500

    chore: configure global ValidationPipe and set port from environment variables in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 8080;
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  app.setGlobalPrefix('api');
+  await app.listen(port);
 }
 bootstrap();
